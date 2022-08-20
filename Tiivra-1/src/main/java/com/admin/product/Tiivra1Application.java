65896d83769c2a0dba382152cb05a49e67500dd2
Praveenkumar M <107694849+praveenjr011@users.noreply.github.com>
2022-08-20 12:31:00 +0530

    Add files via upload
    This my project.. in progress

---
+++ Tiivra-1/src/main/java/com/admin/product/Tiivra1Application.java
@@ -0,0 +1,13 @@
+package com.admin.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tiivra1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tiivra1Application.class, args);
+	}
+
+}
